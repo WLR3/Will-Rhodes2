88c89172aea311cbfd8c69228098840b04d3fd93
WLR3 <rhodeswill37@gmail.com>
2017-06-12 11:21:32 -0500

    initial commit

---
+++ hello-world/hello world.c
@@ -0,0 +1,13 @@
+/*
+ *Will Rhodes
+ * Program 1
+ *  hello world
+ *  06/12/17
+ */
+#include "simpletools.h"                      // Include simple tools
+
+int main()                                    // Main function
+{
+ print("Hello World.\n");
+ print("Hello people of Earth");
+}
